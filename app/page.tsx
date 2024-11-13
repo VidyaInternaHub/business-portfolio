a05204c1328d3bb3fe5bc38456853dc9c56dcbf4
gireesh27 <kasagireesh@gmail.com>
2024-11-14 01:06:05 +0530

    added Testimonials Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import ContactUs from "@/components/Contact/ContactUs";
-
+import Testimonials from "@/components/Testimonials/Testimonials";
 export default function Home() {
   return (
     <>
+      <Testimonials/>
       <ContactUs />
     </>
   );
