522e7704e21c086389a20560259a4ff37f36c2e3
Adesh Singh <adeshsingh3002@gmail.com>
2024-11-14 13:01:18 +0530

    style changes

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,17 +1,15 @@
 interface CardProps {
   children: React.ReactNode;
-  bgColor?: string;
   className?: string;
 }
 
 export default function Card({
   children,
-  bgColor = "bg-accent",
   className,
 }: CardProps) {
   return (
     <div
-      className={`px-4 py-8 ${bgColor} bg-opacity-30 rounded-3xl md:px-8 lg:px-12 transition-all duration-300 ease-in-out ${className}`}
+      className={`border-2 shadow-md bg-opacity-30 rounded-3xl transition-all duration-300 ease-in-out ${className}`}
     >
       {children}
     </div>
