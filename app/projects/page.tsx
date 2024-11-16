fc8ea1be4f0341f2e7e6daa7169b3dac2588b70a
Adesh Singh <adeshsingh3002@gmail.com>
2024-11-16 11:27:03 +0530

    implement project page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Projects from "@/components/Projects/Projects";
+
+export default function Page() {
+    return (
+        <>
+            Hello World 
+            <Projects />
+        </>
+    )
+}
