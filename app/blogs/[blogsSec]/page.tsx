89ae122992738a432d69ccd01051a5e2d937e89a
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2024-11-19 19:07:07 +0530

    Added blogs section

---
+++ app/blogs/[blogsSec]/page.tsx
@@ -0,0 +1,13 @@
+// export default async function Page({
+//   params,
+// }: {
+//   params: Promise<{ projectname: string }>;
+// }) {
+//   const projectname = (await params).projectname.split("-").join(" ").toUpperCase();
+
+//   return (
+//     <>
+      
+//     </>
+//   )
+// }
---
+++ components/blogs/blogs.tsx
@@ -0,0 +1,9 @@
+
+
+export default function blogs(){
+  return<>
+
+  <h1>Hello blogs</h1>
+  
+  </>
+}
