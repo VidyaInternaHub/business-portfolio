d0df2c020fde19988fecf1768af85b47bf6a19c7
Naresh Sharma <naresh.s@vidyainternahub.onmicrosoft.com>
2024-11-20 23:06:44 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-background">
       <Hero />
-      <Projects />
       <Services />
+      <Projects />
       <AboutUs />
       <CaseStudy />
       <Testimonials />
